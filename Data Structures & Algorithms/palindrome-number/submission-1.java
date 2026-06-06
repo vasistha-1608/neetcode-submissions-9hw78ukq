03d2720fcab3c31a199fca1850af5ef888f46a81
vasistha-1608 <68988912+vasistha-1608@users.noreply.github.com>
2026-06-06 16:57:56 -0500

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp =0;
+        int num =x;
+        while(num>0){
+            temp =10*temp + num%10 ;
+            num = num/10;
+            System.out.println(temp);
+        }
+
+        return temp==x;
+    }
+}
